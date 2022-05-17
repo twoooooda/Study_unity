1a1e21173cf011312790c9d20c430c0c0e38f890
twoooooda <yuuyatsuda0602@gmail.com>
2022-05-18 00:43:37 +0900

    add blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,12 +7,17 @@ public class Block : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
     }
 }
