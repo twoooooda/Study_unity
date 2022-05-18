ef449d581e68f076797bbd4e165f14af1bc996ee
twoooooda <yuuyatsuda@yahoo.co.jp>
2022-05-18 17:30:01 +0900

    add BlockBreak scene

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0.2f, 0, 0.2f) * 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = rb.velocity.normalized * speed;
+    }
+}
