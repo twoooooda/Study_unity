8d601993549540020d91a934b999534b4d1178fb
twoooooda <yuuyatsuda0602@gmail.com>
2022-05-21 19:56:19 +0900

    fix scripts

--- Assets/Scripts/Test/BarController.cs
+++ Assets/Scripts/Test/BarController.cs
@@ -6,21 +6,18 @@ public class BarController : MonoBehaviour
 {
     Rigidbody rb;
     public float power;
+    private Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        pos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //キーを離したときにバーを止める
-        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            rb.velocity = Vector3.zero;
-        }
-
         //矢印キー左右でバーを移動
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -28,10 +25,19 @@ public class BarController : MonoBehaviour
             rb.velocity = Vector3.right * power * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //rb.AddForce(new Vector3(-1, 0, 0) * power * Time.deltaTime);
             rb.velocity = Vector3.left * power * Time.deltaTime;
         }
+
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -4, 4);
+        transform.position = pos;
     }
 }
