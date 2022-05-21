6d5e299cce0d430fe427bb86d5b779e47f76cc3c
twoooooda <yuuyatsuda0602@gmail.com>
2022-05-21 18:02:54 +0900

    add particle

--- Assets/Scripts/Test/ShereController.cs
+++ Assets/Scripts/Test/ShereController.cs
@@ -6,9 +6,12 @@ public class ShereController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    public GameObject particle;
+    private ParticleSystem particlesystem;
     // Start is called before the first frame update
     void Start()
     {
+        particlesystem = particle.GetComponent<ParticleSystem>();
         rb = GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(0.2f, 0, 0.2f) * 3);
     }
@@ -17,5 +20,13 @@ public class ShereController : MonoBehaviour
     void Update()
     {
         rb.velocity = rb.velocity.normalized * speed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "block")
+        {
+            Instantiate(particle, collision.transform.position, Quaternion.identity);
+        }
     }
 }
