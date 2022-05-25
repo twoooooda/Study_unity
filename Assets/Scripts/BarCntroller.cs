7b635236834922cb905fffdafdbe1e0aef01d83b
twoooooda <yuuyatsuda@yahoo.co.jp>
2022-05-25 17:19:13 +0900

    add 2nd benkyoukai

---
+++ Assets/Scripts/BarCntroller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarCntroller : MonoBehaviour
+{
+    Rigidbody rb;
+    public float power;
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //矢印キー左右でバーを移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //rb.AddForce(new Vector3(1, 0, 0) * Time.deltaTime);
+            rb.velocity = Vector3.right * power * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //rb.AddForce(new Vector3(-1, 0, 0) * power * Time.deltaTime);
+            rb.velocity = Vector3.left * power * Time.deltaTime;
+        }
+
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -4, 4);
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
