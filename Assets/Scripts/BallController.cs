97ea04e4ebe63be36c1c3a1a145975bf22b571a9
twoooooda <yuuyatsuda0602@gmail.com>
2022-05-28 13:49:40 +0900

    prepare for 3rd benkyoukai

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,17 +6,32 @@ public class BallController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    public GameObject particle;
+    public AudioClip breaksound;
 
-    // Start is called before the first frame update
     void Start()
     {
+        //Rigidbodyを取得し、初速を与える
         rb = GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(0.2f, 0, 0.2f) * 3);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Ballの速度を一定に保つ
         rb.velocity = rb.velocity.normalized * speed;
+    }
+
+    //衝突した瞬間に一度呼ばれる
+    void OnCollisionEnter(Collision collision)
+    {
+        //当たった相手のタグがblockかどうか判定
+        if (collision.gameObject.tag == "block")
+        {
+            //particleを発生させる
+            Instantiate(particle, collision.transform.position, Quaternion.identity);
+            //効果音を鳴らす
+            AudioSource.PlayClipAtPoint(breaksound, collision.transform.position);
+        }
     }
 }
--- Assets/Scripts/Test/ShereController.cs
+++ Assets/Scripts/Test/ShereController.cs
@@ -7,16 +7,13 @@ public class ShereController : MonoBehaviour
     Rigidbody rb;
     public float speed;
     public GameObject particle;
-    private ParticleSystem particlesystem;
-    // Start is called before the first frame update
+
     void Start()
     {
-        particlesystem = particle.GetComponent<ParticleSystem>();
         rb = GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(0.2f, 0, 0.2f) * 3);
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.velocity = rb.velocity.normalized * speed;
