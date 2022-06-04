665b7b23bc31cfabe22b7f85c467208d361383c3
twoooooda <yuuyatsuda0602@gmail.com>
2022-06-04 11:02:51 +0900

    add tmp and scoremanager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Sore : " + score;
+    }
+}
